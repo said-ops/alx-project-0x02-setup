ceb14158c87085125c2bf7aa48cf2298d6f493d2
said-ops <idrissisaid29@gmail.com>
2025-01-12 16:15:13 +0100

    task 6

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -10,6 +10,7 @@ const Users: React.FC = () => {
 
   useEffect(() => {
     // Fetch users from JSONPlaceholder API
+    getStaticProps()
     const fetchUsers = async () => {
       try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
