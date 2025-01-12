107046daac3050f2deb7c381f5874b53d888b9c1
said-ops <idrissisaid29@gmail.com>
2025-01-12 16:11:26 +0100

    task 6

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import React from 'react';
 import Button from '@/components/common/Button';
-import Header from '../components/layout/Header';
+import Header from '@/components/layout/Header';
 
 const About: React.FC = () => {
   return (
